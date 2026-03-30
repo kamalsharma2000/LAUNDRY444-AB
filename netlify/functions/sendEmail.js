c21070d8262777ca661d1dc7b60501a0e40dc4da
kamalsharma2000 <hkamaldeep7@gmail.com>
2026-03-30 17:05:29 -0400

    Redeployement Email111

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -14,14 +14,17 @@ exports.handler = async (event) => {
 
         const SERVICE_ID = process.env.EMAILJS_SERVICE_ID;
         const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;
+        const PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;
         const PRIVATE_KEY = process.env.EMAILJS_PRIVATE_KEY;
         console.log("ENV:", SERVICE_ID, TEMPLATE_ID, PRIVATE_KEY); // 
 
-        const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {            method: "POST",
+        const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
+            method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 service_id: SERVICE_ID,
                 template_id: TEMPLATE_ID,
+                user_id: PUBLIC_KEY,
                 private_key: PRIVATE_KEY,
                 template_params: { name, email, mobile, message }
             })
